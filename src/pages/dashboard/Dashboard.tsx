713729aad3c12283622b2ace00e3ed3bb37f01e9
Corey Mostero <mostero@berkeley.edu>
2024-12-03 20:52:22 -0800

    dashboard mostly complete

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,15 +1,15 @@
 import AuthGuard from "@/components/AuthGuard";
 import DashboardLayout from "@/pages/dashboard/DashboardLayout";
+import DashboardGraph from "@/pages/dashboard/DashboardGraph";
 import LightningBoltIcon from "@/assets/icon/LightningBolt";
-import { ChevronUpIcon, EllipsisIcon, HistoryIcon, SparklesIcon, UploadIcon } from "lucide-react";
+import { ChevronUpIcon, EllipsisIcon, HistoryIcon, PlusIcon, SparklesIcon, UploadIcon } from "lucide-react";
 import React, { MouseEventHandler } from "react";
-import DashboardGraph from "./DashboardGraph";
 
 export default function Dashboard() {
 	return (
 		<AuthGuard>
 			<DashboardLayout>
-				<div className="grid h-full grid-cols-12 grid-rows-[auto_auto_1fr] gap-4 p-10">
+				<div className="grid h-full grid-cols-12 grid-rows-[auto_auto_auto] place-content-between gap-x-4 p-10">
 					<div className="col-span-12 row-span-1">
 						<DashboardHeader title={"Charging Station"} />
 					</div>
@@ -18,10 +18,12 @@ export default function Dashboard() {
 						<DashboardResult />
 					</div>
 
-					<div className="col-span-8 row-span-1">
+					<div className="col-span-7 row-span-1">
 						<DashboardGraph />
 					</div>
-					<div className="col-span-4 row-span-1">{/* Key Performance Indicators */}</div>
+					<div className="col-span-5 row-span-1">
+						<DashboardPerformance />
+					</div>
 				</div>
 			</DashboardLayout>
 		</AuthGuard>
@@ -90,9 +92,67 @@ interface DashboardResultBannerProps {
 
 function DashboardResultBanner({ text }: DashboardResultBannerProps) {
 	return (
-		<div className="flex items-center justify-between rounded bg-[#CCFF0005] px-6 py-4 outline outline-[0.5px] outline-[#C8E972]">
-			<span className="text-[16px] font-medium text-[#C9FF3B]">{text}</span>
+		<div className="flex items-center justify-between rounded bg-[#CCFF0005] px-4 py-4 outline outline-[0.5px] outline-[#C8E972]">
+			<span className="px-2 text-[16px] font-medium text-[#C9FF3B]">{text}</span>
 			<EllipsisIcon size={24} stroke="#C8E972" />
+		</div>
+	);
+}
+
+function DashboardPerformance() {
+	return (
+		<div className="flex h-full w-full flex-col gap-y-4">
+			<div className="flex flex-row items-center justify-between">
+				<span className="text-2xl font-semibold text-[#FDFDFDFD]">Key Performance Indicators</span>
+				<button className="flex flex-row items-center gap-x-2 rounded bg-transparent px-4 py-2 text-base font-medium text-[#FCFCFC] outline outline-1 outline-[#525252]">
+					Variables
+					<PlusIcon size={24} fill="#FCFCFC" />
+				</button>
+			</div>
+			<div className="grid h-full w-full grid-cols-2 grid-rows-2 gap-6">
+				<DashboardPerformanceInfo
+					title="Infrastructure Units"
+					description="This describes variable two and what the shown data means."
+					indicator="€421.07"
+				/>
+				<DashboardPerformanceInfo
+					title="Charging Growth"
+					description="This describes variable two and what the shown data means."
+					indicator="33.07"
+				/>
+				<DashboardPerformanceInfo
+					title="Localization change"
+					description="This describes variable two and what the shown data means."
+					indicator="21.9%"
+				/>
+				<DashboardPerformanceInfo
+					title="Fleet growth"
+					description="This describes variable two and what the shown data means."
+					indicator="7.03%"
+				/>
+			</div>
+		</div>
+	);
+}
+
+interface DashboardPerformanceInfoProps {
+	title: string;
+	description: string;
+	indicator: string;
+}
+
+function DashboardPerformanceInfo(props: DashboardPerformanceInfoProps) {
+	const { title, description, indicator } = props;
+
+	return (
+		<div className="flex aspect-square w-full flex-col justify-between rounded bg-[#222324] p-6 outline outline-1 outline-[#525252]">
+			<div className="flex h-fit w-full flex-col gap-4">
+				<span className="text-lg font-medium text-white">{title}</span>
+				<span className="text-[12px] font-light text-[#BBBBBB]">{description}</span>
+			</div>
+			<div className="flex items-end justify-end">
+				<span className="text-3xl font-bold text-white">{indicator}</span>
+			</div>
 		</div>
 	);
 }
--- src/pages/dashboard/DashboardGraph.tsx
+++ src/pages/dashboard/DashboardGraph.tsx
@@ -1,7 +1,100 @@
+import { ChartContainer, ChartTooltip, type ChartConfig } from "@/components/ui/chart";
+import { CircleArrowUpIcon, CircleHelpIcon } from "lucide-react";
+import { CartesianGrid, Line, LineChart, XAxis, YAxis } from "recharts";
+
 export default function DashboardGraph() {
 	return (
-		<div className="flex flex-col gap-y-4">
+		<div className="flex h-full flex-col justify-center gap-y-6">
 			<span className="text-2xl font-semibold text-[#FDFDFDFD]">Graphs</span>
+			<ChartContainer
+				config={chartConfig}
+				className="h-full min-h-[200px] max-w-full rounded bg-[#222324] p-4 outline outline-1 outline-[#525252]"
+			>
+				<LineChart
+					accessibilityLayer
+					data={chartData}
+					margin={{
+						left: 12,
+						right: 12,
+					}}
+				>
+					<CartesianGrid vertical={false} />
+					<XAxis
+						dataKey="month"
+						tickLine
+						axisLine
+						tickMargin={8}
+						tickFormatter={(value) => value.slice(0, 3)}
+					/>
+					<YAxis dataKey="price" />
+					<ChartTooltip
+						cursor={{ stroke: "#C8E972", strokeDasharray: "3 3" }}
+						content={({ payload }) => {
+							return (
+								<GraphTooltip
+									price={Number.parseFloat(payload?.at(0)?.value?.toString() ?? "0")}
+									description="4.6% above target"
+								/>
+							);
+						}}
+					/>
+					<Line
+						dataKey="price"
+						type="linear"
+						stroke="#C8E972"
+						strokeWidth={3}
+						dot={false}
+						activeDot={{
+							r: 6,
+							stroke: "#C8E972",
+							fill: "#222324",
+							strokeWidth: 3,
+							style: {
+								filter: "drop-shadow(0 0 4px #C8E972)",
+							},
+						}}
+					/>
+				</LineChart>
+			</ChartContainer>
+		</div>
+	);
+}
+
+const chartData = [
+	{ month: "April", price: 20 },
+	{ month: "May", price: 50 },
+	{ month: "June", price: 40 },
+	{ month: "July", price: 90 },
+	{ month: "August", price: 60 },
+	{ month: "September", price: 30 },
+	{ month: "October", price: 60 },
+];
+
+const chartConfig = {
+	desktop: {
+		label: "Desktop",
+		color: "hsl(var(--chart-1))",
+	},
+} satisfies ChartConfig;
+
+interface GraphTooltipProps {
+	price: number;
+	description: string;
+}
+
+function GraphTooltip(props: GraphTooltipProps) {
+	const { price, description } = props;
+
+	return (
+		<div className="flex min-w-[160px] flex-col justify-between gap-y-4 rounded p-5 outline outline-1 outline-[#525252] backdrop-blur-lg">
+			<div className="flex flex-row items-center justify-between">
+				<span className="text-[20px] font-extrabold text-white">{`$${price.toFixed(2)}k`}</span>
+				<CircleHelpIcon size={16} stroke="#888888" />
+			</div>
+			<div className="flex flex-row items-center justify-between gap-x-2">
+				<CircleArrowUpIcon size={18} fill="#C8E97233" stroke="#C8E972" />
+				<span className="text-[16px] font-normal text-[#878787]">{description}</span>
+			</div>
 		</div>
 	);
 }
